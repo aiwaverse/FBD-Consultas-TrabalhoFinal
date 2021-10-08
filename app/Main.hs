740144cfa40fa7b8ca2e9dde89efb3f528917a15
Phi <aiwavision@protonmail.com>
2021-10-08 13:13:02 -0300

    started and made quick test

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,15 @@
 module Main (main) where
 
 import QueriesSQL (projectName)
+import Database.HDBC
+import Database.HDBC.PostgreSQL
+
+something :: IO ()
+something = do 
+              c <- connectPostgreSQL "host=localhost dbname=TBF user=maya password=sapphic"
+              res <- quickQuery c "select * from conta" []
+              print res
 
 
 main :: IO ()
-main = putStrLn ("Executable for " ++ projectName)
+main = something
